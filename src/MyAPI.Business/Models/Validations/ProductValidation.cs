74f582af47ae2aa8280d404b499e0c9ec98213b3
Diego Fernandes <fernandes.diegod@gmail.com>
2021-03-28 17:29:10 -0300

    Implement product service and validation

---
+++ src/MyAPI.Business/Models/Validations/ProductValidation.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyAPI.Business.Models.Validations
+{
+    class ProductValidation : AbstractValidator<Product>
+    {
+        public ProductValidation()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("The field {PropertyName} is required")
+                .Length(2, 200).WithMessage("The field {PropertyName} must be between {MinLenght} and {MaxLenght} characters.");
+
+            RuleFor(x => x.Description)
+                .NotEmpty().WithMessage("The field {PropertyName} is required")
+                .Length(2, 1000).WithMessage("The field {PropertyName} must be between {MinLenght} and {MaxLenght} characters.");
+
+            RuleFor(x => x.Price)
+                .GreaterThan(0).WithMessage("The field {PropertyName} must be greater than {ComparisonValue}");
+        }
+    }
+}
---
+++ src/MyAPI.Business/Services/ProductService.cs
@@ -0,0 +1,44 @@
+﻿using MyAPI.Business.Interfaces;
+using MyAPI.Business.Models;
+using MyAPI.Business.Models.Validations;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyAPI.Business.Services
+{
+    class ProductService : BaseService, IProductService
+    {
+        private readonly IProductRepository _productRepository;
+        private readonly INotifier _notifier;
+        public ProductService(IProductRepository productRepository, INotifier notifier) : base(notifier)
+        {
+            _productRepository = productRepository;
+
+        }
+        public async Task Add(Product product)
+        {
+            if (!ExecuteValidation(new ProductValidation(), product)) return;
+
+            await _productRepository.Add(product);
+        }
+        public async Task Update(Product product)
+        {
+            if (!ExecuteValidation(new ProductValidation(), product)) return;
+
+            await _productRepository.Update(product);
+        }
+        public async Task Remove(Guid id)
+        {
+            await _productRepository.Remove(id);
+        }
+
+        public void Dispose()
+        {
+            _productRepository?.Dispose();
+        }
+
+
+    }
+}
